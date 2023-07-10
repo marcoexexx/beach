81b77c232ac8a7f733447529cdada31487dd2966
marco <toyko20012gmail.com>
2023-07-10 19:18:04 +0630

    init

---
+++ crab.ts
@@ -0,0 +1,34 @@
+import { None, Option, Some } from './option';
+import Result, { Err, Ok } from './result'
+
+
+type MyErrorEnum = 
+  | "NotFound404"
+  | "ServerError500"
+
+
+function calculate(div: number): Result<number, string> {
+  if (div === 0) return Err("not allowed");
+  return Ok(12/div);
+}
+
+
+async function promise(err: boolean): Promise<Result<string, MyErrorEnum>> {
+  const err_data: Option<string> = await Promise.reject().then(d => Some<string>(d)).catch(_ => None<string>())
+  const ok_data: Option<string> = await Promise.resolve("hello, world").then(d => Some<string>(d)).catch(_ => None<string>())
+
+  if (err && err_data.is_some()) return Ok(err_data.unwrap());
+  if (!err && ok_data.is_some()) return Ok(ok_data.unwrap());
+  return Err("ServerError500");
+}
+
+(async function main() {
+  const data = calculate(0).is_ok();
+  const name = Some(20).unwrap();
+
+  console.log({ data, name });
+  const ok_hello = (await promise(false)).unwrap();
+  console.log({ ok_hello });
+  const err_hello = (await promise(true)).unwrap();
+  console.log({ err_hello });
+})()
