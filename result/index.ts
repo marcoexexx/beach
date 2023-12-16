83cd7f84ee0909dee76b091db9ec3e31f0f296ed
Koko Lwin <toyko2001@gmail.com>
2023-12-16 10:07:24 +0630

    done :: result

--- result/index.ts
+++ result/index.ts
@@ -1,11 +1,18 @@
 import { err } from './err'
+import { unwrap_failed } from './excceptions'
 import { ok } from './ok'
 
 export * from './ok'
 export * from './err'
 export * from './as_result'
 
-export class Result<T, E> {
+
+export interface ToString {
+  toString(): string
+}
+
+
+export class Result<T extends ToString, E extends ToString> {
   _type: "ok" | "err"
   value: T | E
 
@@ -46,7 +53,7 @@ export class Result<T, E> {
     return undefined
   }
 
-  map<U>(op: (x: T) => U): Result<U, E> {
+  map<U extends ToString>(op: (x: T) => U): Result<U, E> {
     switch (this._type) {
       case "ok": return ok(op(this.value as T))
       case "err": return err(this.value as E)
@@ -57,6 +64,89 @@ export class Result<T, E> {
     switch (this._type) {
       case "ok": return f(this.value as T)
       case "err": return default_
+    }
+  }
+
+  map_or_else<U>(default_: (err: E) => U, f: (x: T) => U): U {
+    switch (this._type) {
+      case "ok": return f(this.value as T)
+      case "err": return default_(this.value as E)
+    }
+  }
+
+  map_err<U extends ToString>(op: (err: E) => U): Result<T, U> {
+    switch (this._type) {
+      case "ok": return ok(this.value as T)
+      case "err": return err(op(this.value as E))
+    }
+  }
+
+  and<U extends ToString>(res: Result<U, E>): Result<U, E> {
+    switch (this._type) {
+      case "ok": return res
+      case "err": return err(this.value as E)
+    }
+  }
+
+  and_then<U extends ToString>(op: (x: T) => Result<U, E>): Result<U, E> {
+    switch (this._type) {
+      case "ok": return op(this.value as T)
+      case "err": return err(this.value as E)
+    }
+  }
+
+  or<U extends ToString>(res: Result<T, U>): Result<T, U> {
+    switch (this._type) {
+      case "ok": return ok(this.value as T)
+      case "err": return res
+    }
+  }
+
+  or_else<U extends ToString>(op: (err: E) => Result<T, U>): Result<T, U> {
+    switch (this._type) {
+      case "ok": return ok(this.value as T)
+      case "err": return op(this.value as E)
+    }
+  }
+
+  expect(msg: string): T {
+    switch (this._type) {
+      case "ok": return this.value as T
+      case "err": unwrap_failed(msg, this.value as E)
+    }
+  }
+
+  expect_err(msg: string): E {
+    switch (this._type) {
+      case "ok": unwrap_failed(msg, this.value as T)
+      case "err": return this.value as E
+    }
+  }
+
+  unwrap(): T {
+    switch (this._type) {
+      case "ok": return this.value as T
+      case "err": unwrap_failed("called `Result::unwrap()` on an `Err` value", this.value as E)
+    }
+  }
+
+  unwrap_or(default_: T): T {
+    switch (this._type) {
+      case "ok": return this.value as T
+      case "err": return default_
+    }
+  }
+  unwrap_or_else(op: (err: E) => T): T {
+    switch (this._type) {
+      case "ok": return this.value as T
+      case "err": return op(this.value as E)
+    }
+  }
+
+  unwrap_err(): E {
+    switch (this._type) {
+      case "ok": unwrap_failed("called `Result::unwrap_err()` on an `Ok` value", this.value as T) 
+      case "err": return this.value as E
     }
   }
 }
